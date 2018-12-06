d714bdd5abe7569b308bc4454bcf84b150a3fc10
244686710 <244686710@qq.com>
2018-12-06 23:11:58 +0800

    css,img预处理

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.css';
+import './bar.css';
+import './test.less'
+import Img from './dev.png'
+console.log('Hello world');
+// let IMG = document.createElement('img')
+// IMG.src = Img;
+// document.getElementsByTagName('body')[0].appendChild(IMG);
+console.log(Img)
